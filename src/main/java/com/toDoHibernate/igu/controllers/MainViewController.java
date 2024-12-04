749db608e9753fdd0c1ade175d72d5570e4bb4a8
Facundo Villoldo <facundovilloldoyegros@gmail.com>
2024-12-03 22:25:53 -0300

    Fix: fixed current list button

--- src/main/java/com/toDoHibernate/igu/controllers/MainViewController.java
+++ src/main/java/com/toDoHibernate/igu/controllers/MainViewController.java
@@ -1,31 +1,42 @@
 package com.toDoHibernate.igu.controllers;
 
 import com.toDoHibernate.igu.dto.UserLoginDTO;
+import com.toDoHibernate.persistence.entities.ListTasks;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainViewController {
 
     @FXML private Label lblUserEmail;
     @FXML private Label lblUserName;
+    @FXML private Label lblListName;
+    @FXML private Button btnGeneralList;
 
     private UserLoginDTO currentUser;
-
 
     public void initialize(UserLoginDTO currentUser) {
         setCurrentUser(currentUser);
         setUserLabels();
+        setListLabel("General");
+        setButtonGroup(btnGeneralList);
     }
 
     private void setUserLabels(){
         lblUserName.setText(this.currentUser.getUserName());
-        lblUserEmail.setText(currentUser.getEmail());
+        lblUserEmail.setText(this.currentUser.getEmail());
     }
 
-    public void setCurrentUser(UserLoginDTO user) {
+    private void setCurrentUser(UserLoginDTO user) {
         this.currentUser = user;
     }
 
+    private void setListLabel(String nameList){
+        lblListName.setText(nameList);
+    }
 
-
+    private void setButtonGroup(Button buttonGroup){
+        buttonGroup.setDisable(true);
+        buttonGroup.getStyleClass().add("current-group");
+    }
 }

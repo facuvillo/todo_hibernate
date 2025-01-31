e1452e918572d6ed349c82c61fdddd7c5c61045f
Facundo Villoldo <facundovilloldoyegros@gmail.com>
2025-01-31 01:21:41 -0300

    feat: add new task

--- src/main/java/com/toDoHibernate/igu/controllers/CardTaskController.java
+++ src/main/java/com/toDoHibernate/igu/controllers/CardTaskController.java
@@ -1,4 +1,14 @@
 package com.toDoHibernate.igu.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CardTaskController {
+
+    @FXML private Label lblTitleTask;
+
+    public void setLabelTitle(String title) {
+        lblTitleTask.setText(title);
+    }
+
 }
--- src/main/java/com/toDoHibernate/igu/controllers/MainViewController.java
+++ src/main/java/com/toDoHibernate/igu/controllers/MainViewController.java
@@ -5,10 +5,14 @@ import com.toDoHibernate.persistence.entities.ListTasks;
 import com.toDoHibernate.persistence.entities.Task;
 import com.toDoHibernate.persistence.entities.User;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-import java.util.Date;
+import java.io.IOException;
 
 public class MainViewController {
 
@@ -16,15 +20,18 @@ public class MainViewController {
     @FXML private Label lblUserName;
     @FXML private Label lblListName;
     @FXML private Button btnGeneralList;
+    @FXML private TextField inputNewTask;
+    @FXML private VBox vboxCardsTasks;
 
-    private ListDAOImple listDAO = new ListDAOImple();
+    private final ListDAOImple listDAO = new ListDAOImple();
     private User currentUser;
     private ListTasks currentListTask;
 
-    public void initialize(User currentUser) {
+    public void initialize(User currentUser) throws IOException {
         setCurrentUser(currentUser);
         setUserLabels();
         setCurrentList(listDAO.findByIdEager(currentUser.getListTasks().getFirst().getId()));
+        setCardsTasks();
         setListLabel(currentListTask.getTitle());
         setButtonGroup(btnGeneralList);
     }
@@ -50,4 +57,29 @@ public class MainViewController {
         buttonGroup.setDisable(true);
         buttonGroup.getStyleClass().add("current-group");
     }
+
+    // Add new task
+    @FXML
+    private void addNewTask (){
+        String titleNewTask = this.inputNewTask.getText();
+        Task newTask = new Task(null,titleNewTask,"",null,false);
+        currentListTask.getTasks().add(newTask);
+        listDAO.update(currentListTask);
+    }
+
+    // Set tasks cards
+    private void setCardsTasks() throws IOException {
+
+        if (!currentListTask.getTasks().isEmpty()){
+            for (Task task : currentListTask.getTasks()){
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/card-task.fxml"));
+                Pane pane = loader.load();
+                CardTaskController cardTaskController = loader.getController();
+                cardTaskController.setLabelTitle(task.getTitle());
+                vboxCardsTasks.getChildren().add(pane);
+            }
+        }
+
+    }
+
 }
